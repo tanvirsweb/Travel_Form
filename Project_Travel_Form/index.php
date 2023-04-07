50b8fe744cb8e1e36f21a75ff337ce88542f49a6
Tanvir Anjom Siddique <tanvir.anjom.siddique@gmail.com>
2023-04-07 22:09:45 +0600

    add bootstrap 5 and change background
    change background image
    
    Update README.md

--- Project_Travel_Form/index.php
+++ Project_Travel_Form/index.php
@@ -5,7 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css?<?php echo time(); ?>">
+    <!-- 
+    PHP css changes not applied?? 
+    ctrt+F5 to reresh
+
+    Most probably the file is just being cached by the server. You could either disable cache (but remember to enable it when the site goes live), or modify href of your link tag, so the server will not load it from cache.
+    If your page is created dynamically by some language like php, you could add some variable at the end of the href value, like:    
+    <link rel="stylesheet" type="text/css" href="css/yourStyles.css?< ?php echo time(); ?>" />
+    That will add the current timestamp on the end of a file path, so it will always be unique and never loaded from cache. -->
 
     <title>Welcome to Travel Form</title>
 </head>
@@ -38,7 +47,7 @@
             
             //initialize attribute values of table trip
             $name= $_POST['name'];
-            $gender= $_POST['gender'];
+            $gender= $_POST['gender'];//$_post['name'] name=? not id-> input/textarea/selec's
             $age= $_POST['age'];
             $email= $_POST['email'];
             $phone= $_POST['phone'];
@@ -63,7 +72,7 @@
 
 <!-- --------------------------------------------------------------------------- -->
     <!-- <img  class="bgimg" src="img/bgimg.jpg"  alt="error loading image..."> -->
-    <div class="container">
+    <div class="container text-white">
         <h1>Welcome to CoxsBazar Trip form</h1>
         <p>Enter your details and submit this form to confirm your participation in this trip. </p>
         
@@ -73,27 +82,42 @@
                 echo "<p class='submitMsg'>Thank you for submitting this form.We are happy to see you for joining us for the Coxsbazar trip.</p>";
             }
         ?>
-
-        <form action="index.php" method="post">
-            <input type="text" name="name" id="name" placeholder="Enter your name " required>
-
-            <input type="number" name="age" id="age" placeholder="Enter your age" required>
-
-            <input type="text" name="gender" id="gender" placeholder="Enter your gender" required>
-
-            <input type="email" name="email" id="email" placeholder="Enter your email" required>
-
-            <input type="phone" name="phone" id="phone" placeholder="Enter your phone" required>
-
-            <textarea name="desc" id="desc" rows="5" cols="30" placeholder="Enter any other information here"></textarea>
-
-            <button class="btn" type="submit" formmethod="post" formaction="index.php">Submit</button>
-            <!-- <button class="btn">Reset</button> -->
-        </form>
+  
     </div>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-11 m-auto">
+            <form action="index.php" method="post">
+            <input class="form-control mb-2" type="text" name="name" id="name" placeholder="Enter your name " required>
 
+            <input class="form-control mb-2" type="number" name="age" id="age" placeholder="Enter your age" required>
 
+            <!-- <input class="form-control mb-2" type="text" name="gender" id="gender" placeholder="Enter your gender" required> -->
+
+            <select class="form-control mb-2" name="gender">
+                <option>Male</option>
+                <option>Female</option>
+                <option>Ohter</option>
+            </select>
+
+            <input class="form-control mb-2" type="email" name="email" id="email" placeholder="Enter your email" required>
+
+            <input class="form-control mb-2" type="phone" name="phone" id="phone" placeholder="Enter your phone" required>
+
+            <textarea class="form-control mb-2" name="desc" id="desc" rows="5" cols="30" placeholder="Enter any other information here"></textarea>
+
+            <!-- <div class="d-flex justify-content-center">
+                <button class="btn btn-primary" type="submit" formmethod="post" formaction="index.php">Submit</button>
+            </div> -->
+            <!-- text-center: makes button(inline tag) center 
+            but m-auto won't make button center as it's inline.margin:auto centers only block elements -->
+            <button class="btn btn-primary mx-auto d-block" type="submit" formmethod="post" formaction="index.php">Submit</button>
+            
+            <!-- <button class="btn btn-primary mt-2 mx-auto d-block" type="reset">Reset</button> -->
+        </form>
+            </div>
+        </div>
+    </div>
     <script src="js/index.js"></script>
     
 </body>
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WebDevProjects_git
  PHP , Laravel , SQL projects
+ @copyright Tanvir Anjom Siddique
+ RUET CSE 19 ,Bangladesh
